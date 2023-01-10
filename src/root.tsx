1c3fb65024bebdec53a2f0424d528e0c7e99129e
Adi Rizky <adirizky54@gmail.com>
2023-01-10 23:59:29 +0700

    add library tw for reset css forms, set font to inter

--- src/root.tsx
+++ src/root.tsx
@@ -16,13 +16,16 @@ import { trpc, client, queryClient } from "~/utils/trpc";
 
 export default function Root() {
   return (
-    <Html lang="en">
+    <Html lang="en" class="h-full bg-gray-50">
       <Head>
         <Title>Supanotes</Title>
         <Meta charset="utf-8" />
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
       </Head>
-      <Body>
+      <Body class="h-full">
         <trpc.Provider client={client} queryClient={queryClient}>
           <Suspense>
             <ErrorBoundary>
